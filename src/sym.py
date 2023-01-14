5c5be42f25df784fa9f14dff2f10cf6194506770
Arunsp2000 <arunsrinipartha@gmail.com>
2023-01-14 16:38:52 -0500

    Add .gitignore, test cases for sym

--- src/sym.py
+++ src/sym.py
@@ -9,17 +9,21 @@ class SYM:
     def add(self, x):
         if x != "?":
             self.n+=1
-            self.has[x] = 1 + (self.has[x] or 0)
+            if x in self.has:
+                self.has[x]+=1
+            else:
+                self.has[x]=1
             if self.has[x] > self.most:
                 self.most, self.mode = self.has[x], x
     def mid(self):
         return self.mode
 
-    def div(self,e):
+    def div(self):
         def fun(p):
             return p*math.log(p,2)
         e=0
         for i in self.has:
             e+=fun(self.has[i]/self.n)
         return -e
-    
+
+
---
+++ test/test_sym.py
@@ -0,0 +1,16 @@
+from src.sym import SYM
+
+class TestSym:
+
+    def testsym(self):
+
+        inp = ["a","a","a","a","b","b","c"]
+        sym = SYM()
+
+        for i in inp:
+            sym.add(i)
+        return "a" == sym.mid() and round(sym.div(),3) == 1.379
+
+testSym = TestSym()
+
+print(testSym.testsym())
