0b34eb15d9a80ed4dbbf363fc8677795b0a1c061
vishnu-ve56 <vishnuerapalli01@gmail.com>
2023-01-14 16:06:33 -0500

    Add sym.py

--- src/sym.py
+++ src/sym.py
@@ -1,7 +1,25 @@
+import math
 class SYM:
     def __init__(self):
         self.n=0
         self.has={}
         self.most=0
         self.mode= None
-        
+    
+    def add(self, x):
+        if x != "?":
+            self.n+=1
+            self.has[x] = 1 + (self.has[x] or 0)
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x
+    def mid(self):
+        return self.mode
+
+    def div(self,e):
+        def fun(p):
+            return p*math.log(p,2)
+        e=0
+        for i in self.has:
+            e+=fun(self.has[i]/self.n)
+        return -e
+    
