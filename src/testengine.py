54ccbd32398f39fa44f4a31333816968ec0fbc17
Karan Gala <[galakaran47@gmail.com]>
2023-01-17 17:54:33 -0500

    Add test_the.py

--- src/testengine.py
+++ src/testengine.py
@@ -1,13 +1,13 @@
 from test.test_num import TestNum
 from test.test_sym import TestSym
 from test.test_rand import TestRand
-
+from test.test_the import TestThe
 class testengine:
     def __init__(self,the):
         self.the = the
         self.help = ""
         self.fails=0
-        self.testcases= {"sym":["check syms", TestSym().testsym],"num":["check nums", TestNum().testnum],"rand":["generate, reset, regenerate same",TestRand(self.the["seed"]).testrand]}
+        self.testcases= {"sym":["check syms", TestSym().testsym],"num":["check nums", TestNum().testnum],"rand":["generate, reset, regenerate same",TestRand(self.the["seed"]).testrand],"the":["show settings",TestThe().testthe]}
     def concat(self, help):
         self.help+=help
         for i in self.testcases:
--- test/test_the.py
+++ test/test_the.py
@@ -1,7 +1,23 @@
 class TestThe:
-    def __init__(self) -> None:
+    def testthe(self):
         pass
-    def test(self):
-        num1 = NUM()
+    def oo(self,t):
+        print(self.o(t))
+        return t
+    def o(self,t):
+        if type(t)!="something":
+            return str(t)
+        return t
 
 
+'''
+
+
+function oo(t) print(o(t)); return t end --> t; print `t` then return it
+function o(t,isKeys,     fun) --> s; convert `t` to a string. sort named keys. 
+  if type(t)~="table" then return tostring(t) end
+  fun= function(k,v) if not tostring(k):find"^_" then return fmt(":%s %s",o(k),o(v)) end end
+  return "{"..table.concat(#t>0 and not isKeys and map(t,o) or sort(kap(t,fun))," ").."}" end
+
+
+'''
