f091ebf0f55b6e2dd62374358ade54614197130f
Arunsp2000 <arunsrinipartha@gmail.com>
2023-01-22 18:27:48 -0500

    Test Commit revert

--- HW2/test/test_data.py
+++ HW2/test/test_data.py
@@ -4,4 +4,4 @@ class TestData:
 
     def testdata(self):
         data = Data("../data/auto93.csv")
-        return (len(data.rows) == 396) and (data.cols.y[0].w == -1) and (data.cols.x[0].at == 0) and len(data.cols.x) == 4 # Difference Indices in Lua(starts from 1) hence different test case returns
+        return (len(data.rows) == 398) and (data.cols.y[0].w == -1) and (data.cols.x[0].at == 0) and len(data.cols.x) == 4 # Difference Indices in Lua(starts from 1) hence different test case returns
