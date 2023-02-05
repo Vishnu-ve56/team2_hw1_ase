0f5360e0e8e7a10bb98ddf9ff1874bc24e8826c9
Karan Gala <[galakaran47@gmail.com]>
2023-02-05 10:58:18 -0500

    Add repRows and repPlace

--- HW4/src/repgrid.py
+++ HW4/src/repgrid.py
@@ -18,3 +18,33 @@ class repgrid:
         cols.insert(0,s)
         cols[0][len(cols[0])-1]="thingX"
         return Data(cols)
+    
+    def repRows(t, rows, u):
+        rows = copyDefined(rows)
+        for j,s in enumerate(rows[len(rows)-1]):
+            rows[0][j] = rows[0]+":"+s
+            rows[len(rows)-1] = None
+            for n,row in enumerate(rows):
+                if n==0:
+                    u = t.rows[len(rows-n+1)]
+                    row.append(u[len(u)-1])
+        return Data(rows)
+
+    def repPlace(data):
+        n,g = 20,[]
+        for i in range(n):
+            g.append([])
+            for j in range(n):
+                g[i].append(" ")
+        maxy=0
+        print("")
+        for r,row in enumerate(data.rows):
+            c=chr(65+r)
+            print(c,row.cells[-1])
+            x,y=row.x*n//1,row.y*n//1
+            maxy = max(maxy,y)
+            g[y][x]=c
+        print("")
+        for y in range(maxy):
+            oo(g[y])
+
