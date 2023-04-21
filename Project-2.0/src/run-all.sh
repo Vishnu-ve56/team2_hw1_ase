8fa964ee9be4cc158039fe0f54a5fd2106eddc05
Arunsp2000 <arunsrinipartha@gmail.com>
2023-04-21 08:18:18 -0400

    push for final datasets

--- Project-2.0/src/run-all.sh
+++ Project-2.0/src/run-all.sh
@@ -2,15 +2,15 @@
 
 # rm -r ../out/*
 cd ..
-python -m src.main -o 1
-python -m src.main -o 2
-python -m src.main -o 3
-python -m src.main -o 4
-python -m src.main -o 2 -m 0.35
-python -m src.main -o 2 -m 0.22
-python -m src.main -o 2 -m 0.62
-python -m src.main -o 2 -m 0.78
-python -m src.main -o 2 -m 0.92
+# python -m src.main -o 1
+# python -m src.main -o 2
+# python -m src.main -o 3
+# python -m src.main -o 4
+# python -m src.main -o 2 -m 0.35
+# python -m src.main -o 2 -m 0.22
+# python -m src.main -o 2 -m 0.62
+# python -m src.main -o 2 -m 0.78
+# python -m src.main -o 2 -m 0.92
 
 # python -m src.main -o 1 -f ../data/auto2.csv
 # python -m src.main -o 2 -f ../data/auto2.csv
@@ -24,3 +24,12 @@ python -m src.main -o 2 -m 0.92
 # python -m src.main -o 2 -f ../data/coc1000.csv
 # python -m src.main -o 1 -f ../data/coc10000.csv
 # python -m src.main -o 2 -f ../data/coc10000.csv
+
+python -m src.main -o 1 -f ../data/SSM.csv
+python -m src.main -o 2 -f ../data/SSM.csv
+python -m src.main -o 1 -f ../data/SSN.csv
+python -m src.main -o 2 -f ../data/SSN.csv
+python -m src.main -o 1 -f ../data/healthCloseIsses12mths0001-hard.csv
+python -m src.main -o 2 -f ../data/healthCloseIsses12mths0001-hard.csv
+python -m src.main -o 1 -f ../data/healthCloseIsses12mths0011-easy.csv
+python -m src.main -o 2 -f ../data/healthCloseIsses12mths0011-easy.csv
